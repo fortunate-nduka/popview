4ce2bdc9ccc62c394d64e46dfd6f9ffabe66507e
iamfortunate <ogodujoseph@gmail.com>
2021-12-11 22:00:35 +0100

    updates

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -37,8 +37,8 @@ https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
 				// console.log(similarList.results);
 				console.log(castList);
 				setMovie(movieList);
-				setCasts(castList.cast.slice(0, 6));
-				setCrews(castList.crew.slice(0, 8));
+				setCasts(castList.cast.slice(0, 5));
+				setCrews(castList.crew.slice(0, 7));
 				setSimilars(similarList.results.slice(0, 3));
 			} catch (err) {
 				<h1>Something Went Wrong</h1>;
@@ -46,62 +46,66 @@ https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
 		};
 		fetchCast();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [SIMILAR_URL]);
+
+	const handleSimilar = ()=>{
+		window.scrollTo(0, 0);
+	}
 
 	return (
 		<div className='singleMovie '>
-				<div
-					className='carousel'
-					key={movie.id}
-					style={{
-						backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)),url(${IMG_URL + movie.backdrop_path
-							})`,
-					}}
-				>
-					<header className='carousel-header'>
-						<div className='wrapper flex ai-c jc-sb'>
-							<Link to='/'>
-								<div className='singleMovie-logo flex ai-c jc-c'>
-									<KeyboardBackspaceIcon /> Go back to homepage
-								</div>
-							</Link>
-						</div>
-					</header>
-					<div className='wrapper singleMovie-wrapper flex ai-c jc-sb'>
-						<div className='carousel-content'>
-							<div className='carousel-title'> {movie.title} </div>
-							<div className='carousel-timeGenre flex ai-c'>
-								<span className='flex ai-c jc-c'>
-									<StarIcon style={{ color: '#d7ab25' }} />
-									{movie.vote_average}
-								</span>
-								<span className='divider'>|</span>
-								{movie.release_date}
-								<span className='divider'>|</span>
-								<span className='flex ai-c jc-c'>
-									<FavoriteIcon style={{ color: '#d11d1d' }} />
-									{movie.vote_count}
-								</span>
+			<div
+				className='carousel'
+				key={movie.id}
+				style={{
+					backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)),url(${IMG_URL + movie.backdrop_path
+						})`,
+				}}
+			>
+				<header className='carousel-header'>
+					<div className='wrapper flex ai-c jc-sb'>
+						<Link to='/'>
+							<div className='singleMovie-logo flex ai-c jc-c'>
+								<KeyboardBackspaceIcon /> Go back to homepage
 							</div>
-							<div className='carousel-overview'> {movie.overview} </div>
-							<div className='carousel-button flex ai-c'>
-								<button className='red'>
-									<PlaylistAddIcon />
-									Add List
-								</button>
-							</div>
-						</div>
-						<figure className='carousel-figure singleMovie-figure'>
-							<img
-								className='carousel-poster'
-								src={IMG_URL + movie.poster_path}
-								alt=''
-							/>
-						</figure>
+						</Link>
 					</div>
+				</header>
+				<div className='wrapper singleMovie-wrapper flex ai-c jc-sb'>
+					<div className='carousel-content'>
+						<div className='carousel-title'> {movie.title} </div>
+						<div className='carousel-timeGenre flex ai-c'>
+							<span className='flex ai-c jc-c'>
+								<StarIcon style={{ color: '#d7ab25' }} />
+								{movie.vote_average}
+							</span>
+							<span className='divider'>|</span>
+							{movie.release_date}
+							<span className='divider'>|</span>
+							<span className='flex ai-c jc-c'>
+								<FavoriteIcon style={{ color: '#d11d1d' }} />
+								{movie.vote_count}
+							</span>
+						</div>
+						<div className='carousel-overview'> {movie.overview} </div>
+						<div className='carousel-button flex ai-c'>
+							<button className='red'>
+								<PlaylistAddIcon />
+								Add List
+							</button>
+						</div>
+					</div>
+					<figure className='carousel-figure singleMovie-figure'>
+						<img
+							className='carousel-poster'
+							src={IMG_URL + movie.poster_path}
+							alt=''
+						/>
+					</figure>
 				</div>
+			</div>
 			<div className='wrapper'>
-			<div className="section-title">casts</div>
+				<div className="section-title">casts</div>
 				<div className='singleMovie__cast flex ai-s jc-c wrap'>
 					{casts.map((cast) => (
 						<>
@@ -160,28 +164,30 @@ https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
 				<div className="section-title">similar movies</div>
 				<div className='singleMovie__similar flex ai-c jc-c wrap'>
 					{similars.map((similar) => (
-						<div key={similar.id} className='singleMovie__similar-card'>
-							{similar.backdrop_path ? (
-								<img src={IMG_URL + similar.backdrop_path} alt='' />
-							) : (
-								<img src={NO_IMAGE} alt='' />
-							)}
-							<div className='movies-details'>
-								<div className='movies-title'>{similar.title}</div>
-								<div className='movies-info flex ai-c jc-sb'>
-									<span>
-										{similar.release_date &&
-											similar.release_date.slice(0, 4)}
-									</span>
-									<div className='movies-info-icons flex ai-c jc-c'>
+						<Link to={`/movie/${similar.id}`}>
+							<div onClick={handleSimilar} key={similar.id} className='singleMovie__similar-card'>
+								{similar.backdrop_path ? (
+									<img src={IMG_URL + similar.backdrop_path} alt='' />
+								) : (
+									<img src={NO_IMAGE} alt='' />
+								)}
+								<div className='movies-details'>
+									<div className='movies-title'>{similar.title}</div>
+									<div className='movies-info flex ai-c jc-sb'>
 										<span>
-											<StarIcon style={{ color: '#d7ab25' }} />
-											{similar.vote_average}
+											{similar.release_date &&
+												similar.release_date.slice(0, 4)}
 										</span>
+										<div className='movies-info-icons flex ai-c jc-c'>
+											<span>
+												<StarIcon style={{ color: '#d7ab25' }} />
+												{similar.vote_average}
+											</span>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
+						</Link>
 					))}
 				</div>
 			</div>
